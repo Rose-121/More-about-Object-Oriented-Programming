d475db8e2def952390322e6040493dfe79150e7a
Rose <mehrabkhanrose10@gmail.com>
2024-10-13 18:41:28 +0600

    Class

---
+++ Employe.py
@@ -0,0 +1,18 @@
+class Employee:
+    
+    def __init__(self):
+        print("Employee created:")
+        
+    def __del__(self):
+        print("Destructor called")
+        
+def Create_obj():
+    print("Making Object...")
+    
+    obj = Employee()
+    print("Function end...")
+    return obj
+
+print("Calling Creat_obj() function...")
+obj = Create_obj()
+print("Program End...")
